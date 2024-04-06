4e7b5774cbc8bfe3dc13ed27a4cc3969e098b526
Max <maxkryvytskyiwork@gmail.com>
2024-04-06 16:59:25 +0300

    save

--- part_1/Fast_api_lesson/task_2/src/database/db.py
+++ part_1/Fast_api_lesson/task_2/src/database/db.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from src.config.my_config import MyConfig
 import configparser
 
 config = configparser.ConfigParser()
 config.read('src/config/config.ini')
 
-SQLALCHEMY_DATABASE_URL = config.get('Security', 'SQLALCHEMY_DATABASE_URL')
+# SQLALCHEMY_DATABASE_URL = config.get('Security', 'SQLALCHEMY_DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = MyConfig.SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
