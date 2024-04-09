656ca1880abd23e92d4d98fe66b9181ba6307e21
Max <maxkryvytskyiwork@gmail.com>
2024-04-09 04:44:50 +0300

    save

--- part_1/Django_lesson/utils/Load_mongoDB/connect_db.py
+++ part_1/Django_lesson/utils/Load_mongoDB/connect_db.py
@@ -1,13 +1,9 @@
 from mongoengine import connect
-import configparser
+from decouple import config
 
-
-config = configparser.ConfigParser()
-config.read("E:\Git_Files\__Python_GOIT__\__Web_2_0__\Web_HW_9\Load_mongoDB\config.ini")
-
-mongo_user = config.get("DB", "USER")
-mongodb_pass = config.get("DB", "PASSWORD")
-db_name = config.get("DB", "DB_NAME")
-domain = config.get("DB", "DOMAIN")
+mongo_user = config("USER")
+mongodb_pass = config("PASSWORD")
+db_name = config("DB_NAME")
+domain = config("DOMAIN")
 
 connect(host=f"mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority", ssl=True)
